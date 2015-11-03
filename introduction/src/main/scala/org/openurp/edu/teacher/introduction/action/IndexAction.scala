24d4e409939b79c741a0fbf50cdd8efa566cdd08
wenxian.tian <tianwx2003@126.com>
2015-11-03 20:56:58 +0800

    rebase on new openurp

--- introduction/src/main/scala/org/openurp/edu/teacher/introduction/action/IndexAction.scala
+++ introduction/src/main/scala/org/openurp/edu/teacher/introduction/action/IndexAction.scala
@@ -12,31 +12,39 @@ import org.openurp.edu.teacher.introduction.model.Category
 import org.openurp.hr.base.model.Staff
 import org.openurp.hr.info.model.EducationInfo
 import org.openurp.hr.info.model.DegreeInfo
+import org.openurp.platform.api.security.Securities
 
-@action("")
 class IndexAction(entityDao: EntityDao) extends ActionSupport {
 
-  @mapping("{teacherId}")
-  def index(@param("teacherId") teacherId: String): String = {
-    val teacher = entityDao.get(classOf[Staff], Numbers.toLong(teacherId))
+  def index(): String = {
+    val teacher = getStaff
     val categories = entityDao.getAll(classOf[Category])
-    val builder_edu = OqlBuilder.from(classOf[EducationInfo],"eduInfo")
+    val builder_edu = OqlBuilder.from(classOf[EducationInfo], "eduInfo")
     builder_edu.where("eduInfo.staff =:t", teacher)
     builder_edu.where("eduInfo.endOn is null")
-    val eduIfo = entityDao.search(builder_edu)  
-    
-    val builder_degree = OqlBuilder.from(classOf[DegreeInfo],"degreeInfo")
+    val eduIfo = entityDao.search(builder_edu)
+
+    val builder_degree = OqlBuilder.from(classOf[DegreeInfo], "degreeInfo")
     builder_degree.where("degreeInfo.staff =:t", teacher)
     builder_degree.where("degreeInfo.endOn is null")
     val degreeInfo = entityDao.search(builder_degree)
-       
-    
+
     put("teacher", teacher)
     put("categories", categories)
-    
+
     put("eduIfo", eduIfo)
     put("degreeInfo", degreeInfo)
-    
+
     forward()
   }
+  
+  private def getStaff(): Staff = {
+    val staffs = entityDao.search(OqlBuilder.from(classOf[Staff], "s").where("s.code=:code", Securities.user))
+    if (staffs.isEmpty) {
+      throw new RuntimeException("Cannot find staff with code " + Securities.user)
+    } else {
+      staffs.head
+    }
+  }
+
 }
--- introduction/src/main/scala/org/openurp/edu/teacher/introduction/action/IntroductionAction.scala
+++ introduction/src/main/scala/org/openurp/edu/teacher/introduction/action/IntroductionAction.scala
@@ -11,27 +11,34 @@ import java.util.Locale
 import org.beangle.commons.lang.Numbers
 import org.beangle.commons.collection.Order
 import org.openurp.hr.base.model.Staff
-import org.beangle.webmvc.context.ActionContextHelper
 import org.beangle.webmvc.api.annotation.ignore
 import org.beangle.webmvc.api.view.View
+import org.openurp.platform.api.security.Securities
 /**
  * @author xinzhou
  */
 class IntroductionAction extends RestfulAction[Introduction] {
-  
-  
+
+  private def getStaff(): Staff = {
+    val staffs = entityDao.search(OqlBuilder.from(classOf[Staff], "s").where("s.code=:code", Securities.user))
+    if (staffs.isEmpty) {
+      throw new RuntimeException("Cannot find staff with code " + Securities.user)
+    } else {
+      staffs.head
+    }
+  }
+
   protected override def getQueryBuilder(): OqlBuilder[Introduction] = {
     val builder: OqlBuilder[Introduction] = OqlBuilder.from(entityName, shortName)
     populateConditions(builder)
-    builder.where("introduction.teacher.id =:t", 309L)
+    builder.where("introduction.teacher =:s", getStaff)
     builder.orderBy(get(Order.OrderStr).orNull).limit(getPageLimit)
   }
 
   @ignore
   protected override def saveAndRedirect(entity: Introduction): View = {
     if (!entity.persisted) {
-      entity.teacher = new Staff
-      entity.teacher.id = 309L
+      entity.teacher = getStaff
     }
     super.saveAndRedirect(entity)
   }
